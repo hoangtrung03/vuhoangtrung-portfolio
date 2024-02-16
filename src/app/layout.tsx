dc6d55147fe1b41dea84775e9cb4b931e2d59c2f
Vu Hoang Trung - Frontend <hoangtrung@mobcec.com>
2024-02-16 11:11:28 +0700

    refactor(0) update meta viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,7 +59,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
-  maximumScale: 1
+  maximumScale: 5,
+  userScalable: false
 }
 
 export default function RootLayout({
