7fea2da2d536ee6766a95e0b7ae501d8acd12834
Vu Hoang Trung - Frontend <hoangtrung@mobcec.com>
2024-02-16 11:20:59 +0700

    refactor(0) update meta viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { Urbanist } from 'next/font/google'
 import './globals.css'
 
@@ -54,6 +54,13 @@ export const metadata: Metadata = {
   icons: {
     icon: '/icon.png'
   }
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 5,
+  userScalable: true
 }
 
 export default function RootLayout({
