02d8d79226c7003442bb8c1ec39138caa6d21eeb
Vu Hoang Trung - Frontend <hoangtrung@mobcec.com>
2024-02-19 09:35:26 +0700

    chore(0) update info timeline

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,38 +1,30 @@
 import styles from './experience.module.css'
 
 export default function Experience() {
+  const experienceData = [
+    {
+      date: '08/2022 - Now',
+      title: 'Bin Corporation Group',
+      description:
+        '<p>BIN Corporation Group is an international multi-disciplinary company based in Ho Chi Minh City, Vietnam. It is founded in 2009 by an entrepreneur, Mr. Jimmy Lee.</p><p>BIN Corp has offices in Hongkong, Singapore, Lithuania, Canada, USA. Thus, we work with a wide range of corporate clients and individuals across the US-UK, Europe, and Asia.</p><p>BIN Corp develops a tech-applied enterprise service ecosystem with the mission of “Thrive on customer-centricity”. Our fields of operation include business management consulting, financial consulting, digital marketing, e-commerce, travel, technology etc.</p>'
+    }
+  ]
+
   return (
     <>
       <section id='experience'>
         <div className='container pt-10 lg:pt-60'>
           <h2 className='uppercase font-bold text-32 lg:text-48 text-white'>Experience</h2>
           <ul className={styles['timeline']}>
-            <li className={styles['event']} data-date='12:30 - 1:00pm'>
-              <h3>Registration</h3>
-              <p>Get here on time, it&apos;s first come first serve. Be late, get turned away.</p>
-              <p>Get here on time, it&apos;s first come first serve. Be late, get turned away.</p>
-              <p>Get here on time, it&apos;s first come first serve. Be late, get turned away.</p>
-            </li>
-            <li className={styles['event']} data-date='2:30 - 4:00pm'>
-              <h3>Opening Ceremony</h3>
-              <p>
-                Get ready for an exciting event, this will kick off in amazing fashion with MOP & Busta Rhymes as an
-                opening show.
-              </p>
-            </li>
-            <li className={styles['event']} data-date='5:00 - 8:00pm'>
-              <h3>Main Event</h3>
-              <p>
-                This is where it all goes down. You will compete head to head with your friends and rivals. Get ready!
-              </p>
-            </li>
-            <li className={styles['event']} data-date='8:30 - 9:30pm'>
-              <h3>Closing Ceremony</h3>
-              <p>
-                See how is the victor and who are the losers. The big stage is where the winners bask in their own
-                glory.
-              </p>
-            </li>
+            {experienceData.map((item, index) => (
+              <li className={styles['event']} data-date={item.date} key={index}>
+                <h3 className='font-bold text-28 lg:text-32'>{item.title}</h3>
+                <div
+                  dangerouslySetInnerHTML={{ __html: item.description }}
+                  className='mt-2 lg:mt-4 text-14 lg:text-16 text-white'
+                ></div>
+              </li>
+            ))}
           </ul>
         </div>
       </section>
