845742e59fda4772718b8ab456668c0e0812273c
Vu Hoang Trung - Frontend <hoangtrung@mobcec.com>
2024-02-20 14:31:49 +0700

    feat(0) add skill section

--- src/app/_components/Skills/Skills.tsx
+++ src/app/_components/Skills/Skills.tsx
@@ -1,3 +1,35 @@
+import Image from 'next/image'
+import styles from './skills.module.css'
+
 export default function Skills() {
-  return <div className='root'>Skills Component</div>
+  const skillData = [
+    {
+      name: 'html',
+      image: '/assets/images/avatar.webp'
+    }
+  ]
+
+  return (
+    <>
+      <section id='skills'>
+        <div className='container pt-10 lg:pt-60'>
+          <div className='grid grid-cols-3 lg:grid-cols-5 gap-4 lg:gap-8'>
+            {skillData.map((item, index) => (
+              <div data-skill={item.name} className={styles['card-skills']} key={index}>
+                {item?.image && (
+                  <Image
+                    src='/assets/images/avatar.webp'
+                    fill
+                    alt={item.name ? item.name : 'Logo skill' + index}
+                    title={item.name ? item.name : 'Logo skill' + index}
+                    className='object-cover'
+                  />
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
