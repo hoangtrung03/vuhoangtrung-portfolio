f32fe2dfc39ad64f6cae9d808494cceb5bc88af2
Vu Hoang Trung - Frontend <hoangtrung@mobcec.com>
2024-02-20 16:46:57 +0700

    chore(0) update import page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
 import { Suspense, lazy } from 'react'
 
-// const Hero = lazy(() => import('@/app/_components/Hero'))
-// const Experience = lazy(() => import('@/app/_components/Experience'))
-// const Skills = lazy(() => import('@/app/_components/Skills'))
+const Hero = lazy(() => import('@/app/_components/Hero'))
+const Experience = lazy(() => import('@/app/_components/Experience'))
+const Skills = lazy(() => import('@/app/_components/Skills'))
 const Projects = lazy(() => import('@/app/_components/Projects'))
 
 export default function Home() {
   return (
     <>
       <main className='mt-[3.75rem] bg-third w-full duration-200'>
-        <Suspense fallback={<div></div>}>{/* <Hero /> */}</Suspense>
-        <Suspense fallback={<div></div>}>{/* <Experience /> */}</Suspense>
-        <Suspense fallback={<div></div>}>{/* <Skills /> */}</Suspense>
+        <Suspense fallback={<div></div>}>
+          <Hero />
+        </Suspense>
+        <Suspense fallback={<div></div>}>
+          <Experience />
+        </Suspense>
+        <Suspense fallback={<div></div>}>
+          <Skills />
+        </Suspense>
         <Suspense fallback={<div></div>}>
           <Projects />
         </Suspense>
